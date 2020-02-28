6e7a9c50c37788404a2982244625310ea51b9f9d
Adam Nguyen <s15264@pjwstk.edu.pl>
2020-02-28 17:26:27 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ import Button from '@material-ui/core/Button';
 import NavBar from './components/Navbar'
 import Wardrobe from './components/Wardrobe';
 import TodaysSet from './components/TodaysSet';
+import Suitcases from './components/Suitcases';
 import {BrowserRouter , Route, Switch} from "react-router-dom";
 
 function App() {
+
+
   return (
     <BrowserRouter>
       <div className = "bg">  
         <NavBar />
        <Route path = "/Wardrobe" component ={Wardrobe}/>
        <Route path = "/TodaysSet" component ={TodaysSet}/>
+       <Route path = "/Suitcases" component ={Suitcases}/>
 
       </div>
     </BrowserRouter>
---
+++ src/components/Suitcases.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import FetchSuitcases from "./FetchSuitcases";
+export default function  Suitcases() {
+ 
+  
+ 
+        return (
+        
+            <div className="area-2">
+                    <h1>Suitcases</h1>
+                 <div className="area-2">
+                 <FetchSuitcases />
+               
+                 </div>
+          
+            </div>
+        )
+    
+}
+
+
